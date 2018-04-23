8ee31167ab848d3fe0c32f2cb7b54beebd3f5e52
xieshuai <xs1102@sina.com>
2018-04-23 18:43:37 +0800

    增加cookie和ession操作类，该类会在Request中实例化，并可以获取对象，以便进行更多设置。

---
+++ core/module/Cookie.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xieshuai
+ * Date: 18-4-23
+ * Time: 下午4:21
+ */
+
+namespace Core\Module;
+
+
+class Cookie
+{
+
+}
---
+++ core/module/Session.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xieshuai
+ * Date: 18-4-23
+ * Time: 下午4:53
+ */
+
+namespace Core\Module\BaseFunc;
+
+
+class Session
+{
+
+}
